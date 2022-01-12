256425bd48bdc702daa97d1258a24f8ee0a8c624
Mário <mariobigboss@hotmail.com>
2022-01-11 22:07:33 -0300

    adiciona desafio093

---
+++ desafio093.py
@@ -0,0 +1,17 @@
+dados = {'nome': input("Nome do jogador: ")}
+partidas = int(input('Quantas partidas ' + dados['nome'] + ' jogou? '))
+gol = []
+for i in range(0, partidas):
+    listaGol = int(input(f'   Quantos gols na partida {i+1}? '))
+    gol.append(listaGol)
+dados['gols'] = gol
+dados['total'] = sum(gol)
+print('-='*30)
+print(dados)
+print('-='*30)
+for k, v in dados.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-='*30)
+print(f'O jogador {dados["nome"]} jogou {partidas} partidas.')
+for i, j in enumerate(gol):
+    print(f'    => Na partida {i+1}, fez {j} gols.')
